feac640c3afc70d2263047e0dcf852e6c2259bfa
Artem_Svistelnik <artem.svistelnik@gmail.com>
2021-02-25 15:18:15 +0200

    lesson 3 (points 3-5)

--- blog/models.py
+++ blog/models.py
@@ -45,5 +45,6 @@ def save_images(instance, filename):
 
 class PostPoint(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None)
+    post_header = models.CharField(max_length=250, default='HEADER')
     post_point_text = models.TextField(verbose_name='Пункт поста')
     post_images = models.ImageField(upload_to=save_images, blank=True, verbose_name='Изображение пункта',)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render ,get_object_or_404
 # Create your views here.
 from .models import Post, PostPoint
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
+
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 3)  # По 3 статьи на каждой странице.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, возвращаем первую страницу.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
+
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 
 def post_detail(request, year, month, day, post):
